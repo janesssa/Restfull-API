5f76ec0740821268066eb399600b0ee217149c83
janesssa <34273285+janesssa@users.noreply.github.com>
2018-12-15 11:52:11 +0100

    upto mongodb

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+    mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
+var app = express();
+
+
+var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function (req, res) {
+        Book.find(function (err, books) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json(books);
+            }
+        })
+        res.json(responseJson);
+    });
+
+app.use('/api', bookRouter);
+
+app.get('/', function (req, res) {
+    res.send('Welcome to my API1');
+});
+
+app.listen(port, function () {
+    console.log('Running on port: ' + port);
+});
