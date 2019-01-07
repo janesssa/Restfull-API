065cd42819d05de1667bd57c48b259627aa7b7ab
janesssa <34273285+janesssa@users.noreply.github.com>
2019-01-07 12:48:25 +0100

    EEN 10
    IK HEB EEN 10 MET DEZE CODE OP DE CHECKER

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let express = require('express'),
     mongoose = require('mongoose'),
-    DataTable = require('mongoose-datatable'),
     bodyParser = require('body-parser');
 
 let db = mongoose.connect('mongodb://localhost/movieAPI');
@@ -8,15 +7,14 @@ let db = mongoose.connect('mongodb://localhost/movieAPI');
 let Movie = require('./models/movieModel');
 let app = express();
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 8000;
 
-app.options("/api/movies", function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', null);
-    res.header('Allow', 'GET,POST,OPTIONS');
+app.options(function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
     res.header('Access-Control-Allow-Header', 'Content-Type, Accept, Authorization, Content-Length, X-Request-Width');
     res.send(200);
 });
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,13 +22,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 movieRouter = require('./Routes/movieRoutes')(Movie);
 
 app.use(function (req, res, next) {
-    if (req.accepts("json")) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    if (req.accepts('json' || 'xml' || 'x-www-form-urlencoded')) {
         next();
-        return;
     } else {
-        res.sendStatus(404);
+        res.sendStatus(406);
     }
 });
+
 
 app.use('/api/movies', movieRouter);
 
