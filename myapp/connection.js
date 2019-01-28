0045504c1e55137671b73b1eec251cabe7a49d92
janesssa <34273285+janesssa@users.noreply.github.com>
2019-01-28 22:07:17 +0100

    geen werkende front end

--- myapp/connection.js
+++ myapp/connection.js
@@ -1,4 +1,4 @@
-var test = new Vue ({ 
+var test = new App ({ 
     el: "#content",
     data: {
         isHidden: false
@@ -14,21 +14,21 @@ var test = new Vue ({
 
       mounted () {
         axios
-          .get('http://localhost:3000/api/movies')
+          .get('http://145.24.222.197:8000/api/movies')
           .then(response => (this.items = response.data.items))
       }, 
 
       methods: {
 
             detailItem(_id){
-                let uri = 'http://localhost:3000/api/movies/'+ _id;
+                let uri = 'http://145.24.222.197:8000/api/movies/'+ _id;
                 console.log(uri);
 
             },
 
             deleteItem(_id)
             {
-            let uri = 'http://localhost:3000/api/movies/'+ _id;
+            let uri = 'http://145.24.222.197:8000/api/movies/'+ _id;
             
             axios
             .delete(uri, { data: { foo: "bar" } })
@@ -46,21 +46,19 @@ var test = new Vue ({
 
             newItem()
             {
-                let uri = 'http://localhost:3000/api/movies/';
+            let uri = 'http://145.24.222.197:8000/api/movies/';
 
-                let new_item = {
-                    title : this.items.title, 
-                    artist : this.items.author,
-                    genre : this.items.genre,  
-                } 
+            let new_item = {
+                title : this.items.title, 
+                artist : this.items.author,
+                genre : this.items.genre,  
+            } 
 
-                console.log(new_item);
+            console.log(new_item);
 
-                axios.post(uri, new_item)
-                
-                window.location.reload();      
+            axios.post(uri, new_item)
             
-      
+            window.location.reload();      
         }
     } 
 
--- myapp/src/main.js
+++ myapp/src/main.js
@@ -8,7 +8,7 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#content',
   router,
   components: { App },
   template: '<App/>'
